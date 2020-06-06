a864efc79ee60a184e34191ad238d9f09fbc84f8
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-06-06 15:22:21 +0300

    Added Fesson 2

---
+++ 02. Data types if/02. Data types if/Source.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	//unsigned int a = 5000000000;
+	//unsigned int a = -1;
+	//cout << "a = " << a << endl;
+/*
+	short age = 100;
+	cout << age << " years old!" << endl;*/
+
+	/*float distance = 30.801;
+	double distance = 30.800;
+	cout << "Distance " << distance << endl;*/
+
+	/*bool exit = -3;
+	cout << "Exit => " << exit << endl;*/
+
+	//char name[] = "Bill";
+	//cout << "Hello, " << name << endl;
+
+	/*int b = 20, c = 30;
+
+	int result = b + c;
+	cout << "Result = "<<  result <<endl;*/
+
+	// Task Дано три числа. Визначити чи рівні вони між собою.
+
+	//int a = 0, b = 0, c = 0;
+	//cout << "Enter first digit => " << endl;
+	//cin >> a;
+	//cout << "Enter second digit => " << endl;
+	//cin >> b;
+	//cout << "Enter third digit => " << endl;
+	//cin >> c;
+
+	//if (a == b && a == c) {
+	//	cout << "TURE!!!! a = b and a = c => a= " << a << " b= " << b << " c= " << c << endl;
+	//}
+	//else {
+	//	cout << "FALSE!!!!! " << endl;
+	//}
+
+    // Порівняти 3 числа і висзачити найбільне
+
+	int a = 0, b = 0, c = 0;
+	cout << "Enter a => " << endl;
+	cin >> a;
+	cout << "Enter b => " << endl;
+	cin >> b;
+	cout << "Enter c => " << endl;
+	cin >> c;
+
+	if (a > b && a > c) {
+		cout << "A > " << endl;
+	}
+	else if (b > a && b > c) {
+		cout << "B " << endl;
+	}
+	else if (c > a && b < c) {
+		cout << "c " << endl;
+	}
+	else {
+		cout << "a = b and b = c" <<endl;
+	}
+
+	system("pause");
+	return 0;
+}
